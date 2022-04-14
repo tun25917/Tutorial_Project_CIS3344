2d7ea73a105767c673077a6785a831122be653ae
Nathan Marzina <tun25917@temple.edu>
2022-04-14 17:09:56 -0400

    123 post commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ContactForm } from "./ContactForm";
-import { ContactList } from './ContactList';
+import { ContactList } from "./ContactList";
+import { Header } from "./Header";
 
 export const App = () => (
-  <div>
-        <h1>Manly Stanleys</h1>
-        <p>Its where you want to be</p>
-        <ContactForm />
-        <ContactList />
+    <div>
+        <Header />
+        <div className="min-h-full">
+            <div className="max-w-4xl mx-auto p-2">
+                <ContactForm />
+                <ContactList />
+            </div>
+        </div>
     </div>
 );
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    purge: [],
+    darkmode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
